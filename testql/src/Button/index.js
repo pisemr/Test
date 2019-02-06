8478696db14203446fc4acd62e3996b1d4e75c95
Madhura Pise <madhurapi@Madhuras-MacBook-Air.local>
2019-02-06 12:31:07 -0800

    GraphQL UI

--- testql/src/Button/index.js
+++ testql/src/Button/index.js
@@ -17,5 +17,20 @@ const Button = ({
     {children}
   </button>
 );
+const ButtonUnobtrusive = ({
+  children,
+  className,
+  type = 'button',
+  ...props
+}) => (
+  <button
+    className={`${className} Button_unobtrusive`}
+    type={type}
+    {...props}
+  >
+    {children}
+  </button>
+);
 
+export { ButtonUnobtrusive };
 export default Button;
